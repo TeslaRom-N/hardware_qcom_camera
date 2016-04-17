343e43f9e6557878997f7747ff71bf5f79057bb3
Josue Rivera <prbassplayer@gmail.com>
2016-04-17 21:02:12 +0200

    Correctly apply pathmap to camera
    Change-Id: I8438b4380dd6a99acb47fc3d0e771f553c7f840d
    Signed-off-by: Josue Rivera <prbassplayer@gmail.com>
    (cherry picked from commit e2e308a18043f9f2f132134fa53c5e8940d84371)

--- QCamera2/Android.mk
+++ QCamera2/Android.mk
@@ -68,7 +68,7 @@ LOCAL_C_INCLUDES := \
         $(LOCAL_PATH)/util \
         $(LOCAL_PATH)/HAL3 \
         hardware/libhardware/include/hardware \
-        hardware/qcom/media/msm8996/libstagefrighthw \
+        $(call project-path-for,qcom-media)/libstagefrighthw \
         $(TARGET_OUT_HEADERS)/mm-core/omxcore \
         system/core/include/cutils \
         system/core/include/system \
@@ -98,7 +98,7 @@ endif
 LOCAL_C_INCLUDES += \
         $(TARGET_OUT_HEADERS)/qcom/display
 LOCAL_C_INCLUDES += \
-        hardware/qcom/display/msm8996/libqservice
+        $(call project-path-for,qcom-display)/libqservice
 LOCAL_SHARED_LIBRARIES := libcamera_client liblog libhardware libutils libcutils libdl libsync libgui
 LOCAL_SHARED_LIBRARIES += libmmcamera_interface libmmjpeg_interface libui libcamera_metadata
 LOCAL_SHARED_LIBRARIES += libqdMetaData libqservice libbinder
